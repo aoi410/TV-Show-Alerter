5b6b9b30e77aa968f05a3487a2939a877c7f0474
aoi410 <aoi410@users.noreply.github.com>
2017-08-10 09:44:47 +0900

    Initial Commit

---
+++ TV Show Alerter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Show Alerter
+//
+//  Created by MIHO TAKAGI on 2017/08/10.
+//  Copyright © 2017 Aoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
